18875bc8869e0809ab6e9161ebd5cfc6dd21084c
kyle pairish <kpairish@gmail.com>
2024-01-19 16:54:32 -0800

    implemented assertions

--- testbed/src/main.c
+++ testbed/src/main.c
@@ -1,4 +1,5 @@
 #include <core/logger.h>
+#include <core/asserts.h>
 
 int main(void) {
     KFATAL("A test message: %f", 3.14);
@@ -7,5 +8,7 @@ int main(void) {
     KDEBUG("A test message: %f", 3.14);
     KINFO("A test message: %f", 3.14);
     KTRACE("A test message: %f", 3.14);
+
+    KASSERT(1 == 0);
     return 0;
 }
